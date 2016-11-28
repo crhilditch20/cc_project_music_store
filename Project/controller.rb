b730aec191436e16b52373bb8cefd46cd2fab6b5
crhilditch20 <crhilditch20@gmail.com>
2016-11-28 15:33:07 +0000

    add new genre form and genre index plus more styling

--- Project/controller.rb
+++ Project/controller.rb
@@ -3,3 +3,8 @@ require ('sinatra/contrib/all')
 require ('pry-byebug')
 require_relative('album_controller.rb')
 require_relative('artists_controller.rb')
+require_relative('genre_controller.rb')
+
+get '/' do
+  erb(:index)
+end
---
+++ Project/genre_controller.rb
@@ -0,0 +1,21 @@
+require ('sinatra')
+require ('sinatra/contrib/all')
+require ('pry-byebug')
+require_relative('models/artist')
+require_relative('models/album')
+require_relative('models/genre')
+
+get '/genres' do
+  @genres = Genre.all()
+  erb(:"genres/index")
+end
+
+get '/genres/new' do
+  erb(:"genres/new")
+end
+
+post '/genres' do
+  genre = Genre.new(params)
+  genre.save()
+  redirect to("/genres")
+end
