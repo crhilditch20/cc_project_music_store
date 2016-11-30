57269854f2366f3e6e8f5b0de73e1dcfa08df4e2
crhilditch20 <crhilditch20@gmail.com>
2016-11-30 09:42:10 +0000

    add negative integer check

--- Project/db/music_store.sql
+++ Project/db/music_store.sql
@@ -16,7 +16,7 @@ CREATE TABLE albums (
   id SERIAL8 primary key,
   title TEXT NOT NULL,
   artist_id INT8 references artists(id) NOT NULL,
-  quantity INT4,
+  quantity INT4 CHECK (quantity >= 0),
   genre_id INT8 references genres(id) NOT NULL,
-  on_order INT4
+  on_order INT4 CHECK (on_order >= 0)
 );
